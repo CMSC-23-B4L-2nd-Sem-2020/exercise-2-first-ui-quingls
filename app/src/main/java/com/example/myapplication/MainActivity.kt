54a99abb346bf3e2d2b41d1bcb182362ea564990
quingls <qlsabado@up.edu.ph>
2020-01-29 14:55:16 +0800

    feat: added buttons and textViews

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCount: Int = 0
+    private lateinit var mViewCount :TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mViewCount = findViewById(R.id.show_count)
+    }
+
+    fun showToast(view: View) {
+        val toast: Toast = Toast.makeText(this,R.string.toast_message, Toast.LENGTH_SHORT)
+    }
+    fun countUp(view: View) {
+
+        mCount++;
+        if(mViewCount != null )
+            mViewCount.text = mCount.toString()
     }
 }
